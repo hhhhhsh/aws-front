2e406b4c970bf0da95aa74fcb8a499ce9d7d0dec
hhhhhsh <jiajia072518@gmail.com>
2022-07-26 10:49:49 +0900

    .

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === 'localhost') {
   backendHost = 'http://localhost:8080';
 } else {
-  backendHost = 'http://Back1-dev.ap-northeast-2.elasticbeanstalk.com';
+  backendHost = 'https://api.exceltssh6698.click';
 }
 
 export const API_BASE_URL = `${backendHost}`;
